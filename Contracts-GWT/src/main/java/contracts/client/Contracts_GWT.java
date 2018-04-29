710b83c0142f067c518264fe3fef3d680682ef02
kasjanoves <inflames76@gmail.com>
2018-04-29 12:09:04 +0300

    initial

--- Contracts-GWT/src/main/java/contracts/client/Contracts_GWT.java
+++ Contracts-GWT/src/main/java/contracts/client/Contracts_GWT.java
@@ -1,20 +1,7 @@
 package contracts.client;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.collections.map.HashedMap;
 import org.fusesource.restygwt.client.Defaults;
-import org.fusesource.restygwt.client.FailedResponseException;
-import org.fusesource.restygwt.client.Method;
-import org.fusesource.restygwt.client.MethodCallback;
-import org.fusesource.restygwt.client.Resource;
-import org.fusesource.restygwt.client.RestServiceProxy;
-import org.fusesource.restygwt.client.TextCallback;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -28,22 +15,6 @@ public class Contracts_GWT implements EntryPoint {
 	public void onModuleLoad() {
 		
 		Defaults.setServiceRoot("http://localhost:8080/"); 
-		
-		RESTservice restService = GWT.create(RESTservice.class);
-		Resource resource = new Resource(Defaults.getServiceRoot());
-		((RestServiceProxy) restService).setResource(resource);
-		restService.getHello(new TextCallback() {
-			@Override
-			public void onSuccess(Method method, String response) {
-				Window.alert(response);				
-			}
-			@Override
-			public void onFailure(Method method, Throwable exception) {
-				Window.alert(exception.toString()+" "+
-						method.getResponse().getStatusCode());				
-			}
-		});
-		
 		RootPanel.get().add(new ContractsListForm());
 				
 	}
